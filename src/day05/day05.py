27e8a357f64e37698ffbed832e33df233cde2b37
M  P Akhil kumar <madsoulakhil@gmail.com>
2026-02-09 15:25:47 +0530

    intial commit

---
+++ src/day05/day05.py
@@ -0,0 +1,41 @@
+# def great():
+#     print("Hello, welcome to the great day!")
+# great()
+
+
+# def add(a,b):
+#     return(a+b)
+# print(add(5,3))
+
+# x=10
+# def show_value():
+#     x=5
+#     print("the local value is:",x)
+# show_value()
+# print("the global value is:",x)
+
+# icecream = "vanilla"
+# def food():
+#     fruit = "apple"
+#     vegitable = "carrot"
+#     print("fruit:", fruit, "good for health ")
+#     print("vegitable:", vegitable, "good for health ")
+# food()
+# import math 
+# import random 
+# print(math.sqrt(16))
+# print(random.randint(1,10))
+# print(random.sample(range(1,10000), 8))
+# print(random.sample(["apple", "banana", "cherry", "date", "elderberry"], 1))
+# print(random.seed(4))
+# print(random.shuffle(["apple", "banana", "cherry", "date", "elderberry"],1))
+
+########################### task-1 () ####################
+# print("task-1")
+# def calc_rectangle(length,width):
+#     area = length * width
+#     perimeter = 2*(length+ width)
+#     print(f'area: {area}, perimeter: {perimeter}')
+
+# calc_rectangle(6,5)    
+########################### task-2 () ####################
