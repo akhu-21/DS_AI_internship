186c560959543ddaee7f13a3680be7ea19bf1802
M  P Akhil kumar <madsoulakhil@gmail.com>
2026-02-13 23:40:54 +0530

    first commit

--- src/day11/day11.py
+++ src/day11/day11.py
@@ -8,3 +8,44 @@ plt.title("Monthly Revenue Growth")
 plt.xlabel("Month")
 plt.ylabel("Revenue in USD")
 plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+print("Task 1: Correlation Checker (Scatter Plots)")
+study_hours = [1, 2, 3, 4, 5, 6, 7, 8]
+scores = [50, 55, 65, 70, 75, 85, 90, 95]
+plt.scatter(study_hours, scores)
+plt.xlabel("Study_Hours")
+plt.ylabel("Exam Scores")
+plt.title("Study Hours vs Exam Scores")
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
